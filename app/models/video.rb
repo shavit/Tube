956d73c48ea2f4f17cf56dedac27abd86664ca96
shavit <shavit@users.noreply.github.com>
2016-07-12 10:00:51 +0300

    🛳

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,17 +1,17 @@
 class Video < ApplicationRecord
   def related
-    # Video.where("categories ILIKE ?", self.categories)
-    # .limit(24)
-    
-    subquery = Video.select("id, title").where(id: self.id)
-      .as("original_video").to_sql
-    similar = Video.select("videos.*, ts_rank_cd(to_tsvector('english', videos.title), replace(plainto_tsquery(original_video.title)::text, ' & ', ' | ')::tsquery, 8) AS similarity")
-      .from("videos, #{subquery}")
-      .where('videos.id != original_video.id')
-      .order('similarity DESC')
-      .limit(24)
+    Video.where("categories ILIKE ?", self.categories)
+    .limit(24)
 
-    similar
+    # subquery = Video.select("id, title").where(id: self.id)
+      # .as("original_video").to_sql
+    # similar = Video.select("videos.*, ts_rank_cd(to_tsvector('english', videos.title), replace(plainto_tsquery(original_video.title)::text, ' & ', ' | ')::tsquery, 8) AS similarity")
+    # similar = Video.select("videos.*")
+    #   .from("videos, #{subquery}")
+    #   .where("videos.id != #{self.id}")
+    #   .order('similarity DESC')
+    #   .limit(24)
+
   end
 
 end
